1bda83145cb86b0bd4de9275b8db2c880997979e
Thiago Andrade <tja.usa.202@gmail.com>
2023-12-08 00:22:30 -0500

    Create channel message input

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -2,8 +2,11 @@ import { redirectToSignIn } from "@clerk/nextjs";
 
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { redirect } from "next/navigation";
+
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
+
+import { redirect } from "next/navigation";
 
 interface ChannelIdPageProps {
   params: {
@@ -37,6 +40,16 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
   );
