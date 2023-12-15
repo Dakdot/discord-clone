ab5686d15d2e744fc749333b4e9ed1a1ba992e06
Thiago Andrade <tja.usa.202@gmail.com>
2023-12-15 16:31:59 -0500

    Create socket hook

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -10,7 +10,7 @@ import { ServerCrash } from "lucide-react";
 import { ChatItem } from "./chat-item";
 
 import { RotatingLines } from "react-loader-spinner";
-import { tr } from "date-fns/locale";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "d MMM yyyy HH:mm";
 
@@ -44,6 +44,8 @@ export const ChatMessages = ({
   type,
 }: ChatMessagesProps) => {
   const queryKey = `chat:${chatId}`;
+  const addKey = `chat:${chatId}:messages`;
+  const updateKey = `chat:${chatId}:messages:update`;
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
     useChatQuery({
@@ -52,6 +54,8 @@ export const ChatMessages = ({
       paramKey,
       paramValue,
     });
+
+  useChatSocket({ queryKey, addKey, updateKey });
 
   if (status === "pending") {
     return (
